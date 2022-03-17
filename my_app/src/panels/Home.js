3ffbb5c4528ac01cec557f9bb751f27d00555b7c
210-03\210-03 <210>
2022-03-17 20:25:04 +0500

    Вкладки и сообщение (но не отправить, ток написать)

--- my_app/src/panels/Home.js
+++ my_app/src/panels/Home.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useState, Fragment} from 'react';
 import PropTypes from 'prop-types';
 
-import { Panel, PanelHeader, Header, Button, Group, Cell, Div, Avatar } from '@vkontakte/vkui';
+import { Panel, PanelHeader, Header, Button, Group, Cell, Div, Avatar, CardGrid,ContentCard, FixedLayout, Separator,WriteBar, WriteBarIcon } from '@vkontakte/vkui';
 
-const Home = ({ id, go, fetchedUser }) => (
+
+
+
+const Home = ({ id, go, fetchedUser }) => {
+	const [text, setText] = useState(""); 
+
+	return (
 	<Panel id={id}>
-		<PanelHeader>Example</PanelHeader>
-		{fetchedUser &&
-		<Group header={<Header mode="secondary">User Data Fetched with VK Bridge</Header>}>
+
+
+		<PanelHeader>Люль</PanelHeader>
+		
+		 {/*fetchedUser &&
+		<Group header={<Header mode="secondary">Добро пожаловать!</Header>}>
 			<Cell
 				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
 				description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
@@ -22,9 +31,63 @@ const Home = ({ id, go, fetchedUser }) => (
 					Show me the Persik, please
 				</Button>
 			</Div>
-		</Group>
-	</Panel>
-);
+		</Group> 
+*/} 
+
+		<Group>
+		<CardGrid size="l">
+		  <ContentCard
+			subtitle="VKUI"
+			header="ContentCard example"
+			caption="VKUI Styleguide > Blocks > ContentCard"
+		  />
+		  <ContentCard
+			onClick={() => {}}
+			src="https://images.unsplash.com/photo-1603988492906-4fb0fb251cf8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80"
+			subtitle="unsplash"
+			header="brown and gray mountains under blue sky during daytime photo"
+			text="Mountain changji"
+			caption="Photo by Siyuan on Unsplash"
+			maxHeight={150}
+		  />
+		  <ContentCard
+			disabled
+			src="https://images.unsplash.com/photo-1603928726698-a015a1015d0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
+			subtitle="unsplash"
+			header="persons left hand with pink paint"
+			text="Five hours of makeup and paint to achieve the human anatomy photoshoot. Thank you Steph and Shay. See more and official credit on @jawfox.photography."
+			caption="Photo by Alexander Jawfox on Unsplash"
+			maxHeight={500}
+		  />
+
+          <FixedLayout vertical="bottom">
+        
+            <Separator wide />
+
+           
+            <WriteBar
+            
+              after={
+                <Fragment>
+				  <WriteBarIcon mode="send" />
+                </Fragment>
+              }
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+           
+              placeholder="Сообщение"
+            />
+          
+        </FixedLayout>
+
+		</CardGrid>
+	  </Group>
+
+
+
+	</Panel> )
+
+};
 
 Home.propTypes = {
 	id: PropTypes.string.isRequired,
